f47d3942119876444b3455b4208267b0d7aae549
macnemoto <marcosj465@gmail.com>
2026-03-27 13:18:24 -0400

    first card arrangements

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,19 @@ function App() {
 
   const avatar = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"
   const name = "Marcos Jose"
-  const description = "developer full stack"
-  const button = "contact me"
+  const description = "Software Developer"
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <div className="card bg-[#1E1E1F] rounded-lg p-4 flex flex-row">
+      <div className="bg-[#1E1E1F] rounded-lg p-4 flex flex-row gap-5">
         <div className='flex items-center justify-center h-24 bg-[#353535] rounded-lg'>
           <img className='w-20 h-20' src={avatar} alt="avatar" />
         </div>
         <div>
-          <h2>{name}</h2>
-          <p>{description}</p>
-          <button>{button}</button>
+          <h2 className='mb-2'>{name}</h2>
+          <div className='flex items-center justify-center w-full h-10 pl-1 pr-1 bg-[#353535] rounded-lg'>
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </>
