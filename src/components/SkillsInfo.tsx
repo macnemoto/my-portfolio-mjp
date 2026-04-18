a45514b68aec3cb6c4c6d9d0ff57534317dc1758
macnemoto <marcosj465@gmail.com>
2026-04-18 10:03:12 -0400

    refactor card code

--- src/components/SkillsInfo.tsx
+++ src/components/SkillsInfo.tsx
@@ -1,19 +1,28 @@
+const skills = [
+    { name: 'html5', style: 'plain-wordmark', isColored: true },
+    { name: 'css3', style: 'plain-wordmark', isColored: true },
+    { name: 'javascript', style: 'plain', isColored: true },
+    { name: 'react', style: 'original-wordmark', isColored: true },
+    { name: 'tailwindcss', style: 'original', isColored: true },
+    { name: 'nodejs', style: 'plain-wordmark', isColored: true },
+    { name: 'express', style: 'original-wordmark', isColored: false },
+    { name: 'mongodb', style: 'plain-wordmark', isColored: true },
+    { name: 'git', style: 'plain-wordmark', isColored: true },
+    { name: 'python', style: 'plain-wordmark', isColored: true },
+]
+
+
 function SkillsInfo() {
+
+
     return (
         <div>
             <h1 className="font-bold text-2xl mb-7">Habilidades</h1>
             <ul className="flex flex-row gap-5 overflow-x-auto my-2">
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-html5-plain-wordmark colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-css3-plain-wordmark colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-javascript-plain colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-react-original-wordmark colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-tailwindcss-original colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-nodejs-plain-wordmark colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-express-original-wordmark text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-mongodb-plain-wordmark colored text-7xl"></i></li>
-                <li className="bg-[#242938] p-5 rounded-lg"><i className="devicon-git-plain-wordmark colored text-7xl"></i></li>
+                {skills.map((skill, index) => (
+                    <li key={index} className="bg-[#242938] p-5 rounded-lg"><i className={`devicon-${skill.name}-${skill.style} ${skill.isColored ? 'colored' : ''} text-7xl`}></i></li>
+                ))}
             </ul>
-
         </div>
     );
 }
