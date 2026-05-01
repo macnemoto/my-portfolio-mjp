871f3b6527b697535a4becd2d6b8b4481d8b162b
macnemoto <marcosj465@gmail.com>
2026-05-01 19:52:39 -0400

    Adding the textArea component and improving the form

--- src/features/form/FormCard.tsx
+++ src/features/form/FormCard.tsx
@@ -2,6 +2,8 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { contactSchema, type ContactFormData } from "../../types/form.schema";
 import InputForm from "../../components/InputForm";
+import { Send } from "lucide-react";
+import TextArea from "../../components/TextArea";
 
 function FormCard() {
     const { register, handleSubmit, reset, formState: { errors } } = useForm<ContactFormData>({
@@ -19,12 +21,12 @@ function FormCard() {
     return (
         <section className="bg-primary-color p-5 border border-zinc-800 rounded-lg text-[#75756D]">
             <h2 className="font-bold text-2xl text-white mb-5">Formulario de Contacto</h2>
-            <form onSubmit={handleSubmit(handleSubmitForm)}>
+            <form className="flex flex-col gap-5" onSubmit={handleSubmit(handleSubmitForm)}>
                 <InputForm placeholder={"Nombre de usuario"} errors={errors} name={"fullName"} register={register}/>
                 <InputForm placeholder={"Email"} errors={errors} name={"email"} register={register}/>
-                <InputForm placeholder={"subject"} errors={errors} name={"subject"} register={register}/>
-                <InputForm placeholder={"message"} errors={errors} name={"message"} register={register}/>
-                <button className="my-5 w-full border p-2 rounded-lg" type="submit">Enviar Mensaje</button>
+                <InputForm placeholder={"Sujeto"} errors={errors} name={"subject"} register={register}/>
+                <TextArea placeholder={"Introduce tu mensaje..."} errors={errors} name={"message"} register={register} size=""/>
+                <button className="my-5 w-full border border-zinc-600 p-2 rounded-lg flex gap-3 justify-center text-yellow-300 py-3" type="submit"><Send /> Enviar Mensaje</button>
             </form>
         </section>);
 }
