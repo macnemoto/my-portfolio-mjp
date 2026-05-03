8f072b574f5589ccdf103ce176811e29f8e0fd66
macnemoto <marcosj465@gmail.com>
2026-05-03 12:41:15 -0400

    Creating the function to send the emails and minor corrections to the form names

--- src/features/form/FormCard.tsx
+++ src/features/form/FormCard.tsx
@@ -12,20 +12,36 @@ function FormCard() {
     })
 
     const handleSubmitForm = async (data: ContactFormData) => {
-        console.log("Data lista para enviar", data)
-        await new Promise(res => setTimeout(res, 2000))
-        alert("Mensaje Enviado con Exito")
-        reset()
+        try {
+            const response = await fetch("https://formspree.io/f/mwvypwzr", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(data),
+            })
+            if (response.ok) {
+                alert("¡Mensaje enviado con éxito! Revisaré tu correo pronto.")
+                reset()
+            } else {
+                alert("Hubo un problema al enviar. Por favor, intenta de nuevo.")
+            }
+        } catch (error) {
+            console.error("Detalles del error:", error)
+            alert("Error de conexión. Revisa tu internet.")
+
+        }
     }
 
     return (
         <section className="bg-primary-color p-5 border border-zinc-800 rounded-lg text-[#75756D]">
             <h2 className="font-bold text-2xl text-white mb-5">Formulario de Contacto</h2>
             <form className="flex flex-col gap-5" onSubmit={handleSubmit(handleSubmitForm)}>
-                <InputForm placeholder={"Nombre de usuario"} errors={errors} name={"fullName"} register={register}/>
-                <InputForm placeholder={"Email"} errors={errors} name={"email"} register={register}/>
-                <InputForm placeholder={"Sujeto"} errors={errors} name={"subject"} register={register}/>
-                <TextArea placeholder={"Introduce tu mensaje..."} errors={errors} name={"message"} register={register} size=""/>
+                <InputForm placeholder={"Nombre de usuario"} errors={errors} name={"fullName"} register={register} />
+                <InputForm placeholder={"Correo"} errors={errors} name={"email"} register={register} />
+                <InputForm placeholder={"Asunto"} errors={errors} name={"subject"} register={register} />
+                <TextArea placeholder={"Introduce tu mensaje..."} errors={errors} name={"message"} register={register} size="" />
                 <button className="my-5 w-full border border-zinc-600 p-2 rounded-lg flex gap-3 justify-center text-yellow-300 py-3" type="submit"><Send /> Enviar Mensaje</button>
             </form>
         </section>);
