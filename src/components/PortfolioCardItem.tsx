f632745017cf10ee76c66bdd40a652cbea2ef1a5
macnemoto <marcosj465@gmail.com>
2026-05-25 01:28:54 -0400

    Add grid layout, project cards styling, and section header to Portfolio

--- src/components/PortfolioCardItem.tsx
+++ src/components/PortfolioCardItem.tsx
@@ -5,12 +5,14 @@ import type { PortfolioProps } from "../types/resume.types";
 function PortfolioCardItem({ projects }: PortfolioProps) {
 
     return (
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {projects.map((project) => (
-                <section key={project.id} className="mb-5">
+                <section key={project.id} className="mb-5 border rounded-lg border-slate-800 h-72">
                     <img className="rounded-lg h-44 w-full object-cover mb-5" src={project.image} alt={project.title} />
-                    <h2 className="font-bold">{project.title}</h2>
-                    <p className="font-light">{project.type}</p>
+                    <div className="p-4">
+                        <h2 className="font-bold">{project.title}</h2>
+                        <p className="font-light">{project.type}</p>
+                    </div>
                 </section>
             ))}
         </div>
--- src/features/portfolio/PortfolioCard.tsx
+++ src/features/portfolio/PortfolioCard.tsx
@@ -4,7 +4,9 @@ import { projects } from "../../data/portfolio.json";
 
 function PortfolioCard() {
     return (
-        <section className="bg-primary-color p-5 border border-zinc-800 rounded-lg mb-5">
+        <section className="bg-primary-color p-7 border border-zinc-800 rounded-lg mb-5">
+            <h1 className="mb-2 font-bold text-2xl md:text-3xl">My Projects</h1>
+            <p className="text-[#A1A1AA] mb-5">Trabajos recientes</p>
             <PortfolioCardItem projects={projects}/>
         </section>);
 }
